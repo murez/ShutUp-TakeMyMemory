f6f0ec52aa7731cf56ad37235dc49e72dead87d2
murez <ottozsy@outlook.com>
2021-03-05 20:45:55 +0800

    Update getGPU.py

--- getGPU.py
+++ getGPU.py
@@ -9,12 +9,13 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser(description='Get Your GPU InTime')
 parser.add_argument('--code', type=str, default = None, required=False, help=r'Write your exec code like "python train.py".')
 parser.add_argument('--memory', type=int, default = 8000, required=False, help=r'The memory you need, 8000 by default.')
+parser.add_argument('--time', type=int, default = 3600, required=False, help=r'The time you need, 3600 by default.')
 parser.add_argument('--get-all-resource', action='store_true', help=r'Just to get all resources without running anything or the code.')
 ratio = 1024**2
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    
+
     pynvml.nvmlInit()
     while 1:
         deviceNum = pynvml.nvmlDeviceGetCount()
@@ -26,12 +27,14 @@ if __name__ == '__main__':
             free = meminfo.free/ratio
             if free >= args.memory:
                 if args.get_all_resource:
-                    block_mem = int(float(free) * 0.9)
+                    os.environ["CUDA_VISIBLE_DEVICES"]= f"{i}"
+                    block_mem = min(int(float(free) * 0.95), args.memory)
                     x = torch.cuda.FloatTensor(256,1024,block_mem)
-                    del x
-                    for _ in tqdm(range(60)):
+
+                    for _ in tqdm(range(args.time)):
                         time.sleep(1)
-            
+                    del x
+
                 localtime = time.asctime(time.localtime(time.time()))
                 print(f"your code started at {localtime}")
                 os.environ["CUDA_VISIBLE_DEVICES"]= f"{i}"
